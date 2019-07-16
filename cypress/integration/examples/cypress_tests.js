5168d1315b08754b0c92303e8d197e1c45ef1175
hash-browns <52944361+hash-browns@users.noreply.github.com>
2019-07-15 23:03:46 -0600

    Update cypress_tests.js

--- cypress/integration/examples/cypress_tests.js
+++ cypress/integration/examples/cypress_tests.js
@@ -1,6 +1,6 @@
-describe('The home page', function() {
+describe('Home Page Suite', function() {
   
-  it('loads all 3 package cards', function() {
+  it('Loads all 3 package cards', function() {     //3lL31l4BXmLfUZA61SSu3T is the class applied to the 3 pacakges cards
   	cy.visit('https://havenly.com/')
        cy.get(':nth-child(1) > ._3lL31l4BXmLfUZA61SSu3T').contains('Havenly Consult')
        cy.get(':nth-child(2) > ._3lL31l4BXmLfUZA61SSu3T').contains('Havenly Mini')
@@ -8,7 +8,7 @@ describe('The home page', function() {
      
   })
 
-  it('loads in less than 3.5', function() {
+  it('Loads in less than 3.5s', function() {
   	cy.visit('https://havenly.com/',{ timeout: 3500 }) //This will fail since not all stylesheets and scripts are finished loading before cypress receives the 'load' event
   })
 
